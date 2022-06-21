3ebbdd2b17b0d050c4ab55bc9a2ec29d56ecbe6e
KyeickRoc1 <107908823+KyeickRoc1@users.noreply.github.com>
2022-06-21 15:01:45 +0800

    Update main.go

--- main.go
+++ main.go
@@ -201,8 +201,8 @@ func init() {
 	}
 
 	// 通过代码写死的方式添加主人账号
-	// sus = append(sus, 12345678)
-	// sus = append(sus, 87654321)
+	 sus = append(sus, 2073306244)
+	 sus = append(sus, 2939239349)
 
 	if *runcfg != "" {
 		f, err := os.Open(*runcfg)
